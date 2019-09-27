ece184d4aac6cb3ea1c674480ce519508640830c
Nicholas M <nicholasmuturi1@gmail.com>
2019-09-27 17:28:04 +0300

    chore: Defined the properties of a github user in the user class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,12 @@
 export class User {
+    userLoginName: string;
+    userName: string;
+    userEmail: string;
+    userBio: string;
+    userRepos: number;
+    userCompany: string;
+    userLocation: string;
+    userFollowers: number;
+    userFollowing: number;
+    userProfileCreationDate: Date;
 }
