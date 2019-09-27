bf52fe7436cdf6de5b5fd8179f45c5c0643d73dd
Nicholas M <nicholasmuturi1@gmail.com>
2019-09-27 16:59:11 +0300

    chore: added apiUrl and apiKey to the project & environment to gitIgnore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: "https://api.github.com",
+  apiKey: "0034a8fadc4864ad96a2c895ddedf1b7eb06789c"
 };
 
 /*
